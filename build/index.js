9d96dc558d95c1cfa1c3977871540314ee2e0d60
answer <answerxox@gmail.com>
2020-10-31 10:30:04 -0400

    lol ok that should work

--- build/index.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const Bot_1 = require("./Bot");
-// eslint-disable-next-line no-new
-new Bot_1.default('NjM0OTIzOTA2MzY4NjY3Njgx.Xapk9w.eHfrXKsngWc9VPCKTZGhbAM41hQ');
-//# sourceMappingURL=index.js.map
